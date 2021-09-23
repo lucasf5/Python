63af97471576d30a30ff51b6ea590f90c5a37c9f
Lucas Ferreira <lucasfpnt@gmail.com>
2021-09-22 22:40:39 -0300

    Create Ex54.py

---
+++ Ex54.py
@@ -0,0 +1,15 @@
+from datetime import date
+# Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+atual = date.today()
+print(atual)
+anoatual = atual.year
+
+count = 0
+
+for i in range(1,8):
+  data = int(input('Ano de nascimento: '))
+  maioridade = anoatual-data 
+  if maioridade >= 18:
+    count += 1
+
+print(f'{count} pessoas estão na maioridade')
